1466ba47f8b2fa91ba6d1ea612ab6b3afce21a94
fafaew <rafael.gomest@gmail.com>
2021-10-15 17:28:19 -0300

    Initial commit - Added CSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+
+document.title='React Dashboard'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
