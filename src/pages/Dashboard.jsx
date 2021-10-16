bb0586be518c005af9e0e229c1672f2155abc1ca
fafaew <rafael.gomest@gmail.com>
2021-10-16 09:17:49 -0300

    Added Apex Charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import Chart from 'react-apexcharts'
+
 import StatusCard from '../components/status-card/StatusCard'
 
 import statusCards from '../assets/JsonData/status-card-data.json'
+
+const chartOptions = {
+  series: [{
+    name: 'Online Customers',
+    data: [40,70,20,90,36,80,30,91,60,]
+  }, {
+    name: 'Srote Customers',
+    data: [40,30,70,80,40,16,40,20,50,51,10]
+  }],
+  options: {
+    color: ['#6ab04c', '#2980b9'],
+    chart: {
+      background: 'transparent'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+    },
+    legend: {
+      position: 'top'
+    },
+    grid: {
+      show: false
+    }
+  }
+}
 
 const Dashboard = () => {
   return (
@@ -29,6 +64,25 @@ const Dashboard = () => {
         <div className="col-6">
           <div className="card full-height">
             {/* chart */}
+            <Chart
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type='line'
+              height='100%'
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>top customers</h3>
+            </div>
+            <div className="card__body">
+              {/* table */}
+            </div>
+            <div className="card__footer">
+              <Link to='/'>viel all</Link>
+            </div>
           </div>
         </div>
       </div>    
