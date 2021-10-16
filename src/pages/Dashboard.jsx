2e4e91fae5557149b7c76a78529207022b2665f4
fafaew <rafael.gomest@gmail.com>
2021-10-15 22:33:11 -0300

    Added Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import statusCards from '../assets/JsonData/status-card-data.json'
+
 const Dashboard = () => {
   return (
     <div>
-      Dashboard      
+      <h2 className="page-header">Dashboard</h2>  
+      <div className="ro">
+        <div className="col-6">
+          <div className="row">
+            {
+              statusCards.map((item, index) =>(
+                <div className="col-6">
+                  {/* status card here */}
+                  {item.title}
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>    
     </div>
   )
 }
