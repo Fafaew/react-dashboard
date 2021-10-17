5349f49908f50a72986af0126420501bb048f01d
fafaew <rafael.gomest@gmail.com>
2021-10-17 09:39:28 -0300

    Setup Themes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals'
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './assets/css/grid.css'
 import './assets/css/index.css'
+import './assets/css/theme.css'
 
 import Layout from './components/layout/Layout'
 
