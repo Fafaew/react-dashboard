22f4e20504e612d40b32954a24a76fc5c26b8836
fafaew <rafael.gomest@gmail.com>
2021-10-16 21:02:00 -0300

    Added badge

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import Chart from 'react-apexcharts'
 
 import StatusCard from '../components/status-card/StatusCard'
 
 import Table from '../components/table/Table'
 
+import Badge from '../components/badge/Badge'
+
 import statusCards from '../assets/JsonData/status-card-data.json'
-import { Link } from 'react-router-dom'
 
 
 
@@ -149,7 +152,7 @@ const renderOrderBody = (item, index) => (
     <td>{item.price}</td>
     <td>{item.date}</td>
     <td>
-      <span>{item.status}</span>
+      <Badge type={orderStatus[item.status]} content={item.status}/>
     </td>
   </tr>
 )
