2913627b84b372f1b84c4d4599170223209248f7
fafaew <rafael.gomest@gmail.com>
2021-10-18 10:14:42 -0300

    General: update json data and fix apex charts legends

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -49,7 +49,7 @@ const TopNav = () => {
   return (
     <div className='topnav'>
       <div className="topnav__search">
-        <input type="text" placeholder='Search here ...' />
+        <input type="text" placeholder='Search' />
         <i className='bx bx-search'></i>
       </div>
       <div className="topnav__right">
